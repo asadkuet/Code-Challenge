7593b68759043b9433717d08eef3b65b8b184d47
Md. Asadujjaman Nur <asadece2k10@gmail.com>
2021-10-01 21:46:46 +0600

    Updated PHP-HTML challenge

--- 1. PHP-HTML challenge.php
+++ 1. PHP-HTML challenge.php
@@ -1,16 +1,18 @@
 <?php   
-class Person extends ArrayObject {
-	
+class Person extends Person {
+	// set Key, Value to the instance
 	public function __set($name, $val) {
 		$this[$name] = $val;
 	}
 
 	public function displayAsTable() {
+		//Create iterator from parent class
 		$iterator = $this->getIterator(); 
 		
 		$table =  '<table style="border-collapse: collapse">';
 		$table .= '<tbody>';  
 		
+		//iterate through the iterator
 		while($iterator->valid()) {		
 			$table .= '<tr>';
 			$table .= '<th style="padding: 5px; border: 1px solid #ddd">' . $iterator->key() . '</th>';
@@ -25,10 +27,15 @@ class Person extends ArrayObject {
 	} 
 }
 
+// make instance of Person class which is inherited from Person class
 $obj = new Person();    
+
+//set some keys & values
 $obj->Name = 'Md. Asadujjaman Nur'; 
 $obj->Email = 'asadece2k10@gmail.com'; 
 $obj->Mobile = '+8801740227805'; 
+
+// display the data
 echo $obj->displayAsTable();
 
 ?>
